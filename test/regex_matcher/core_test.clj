103e50f7f86a0a29dd332d5e6d21fca468028ae0
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2018-06-04 19:03:07 +0300

    Added test for a slightly more complicated dfa

--- test/regex_matcher/core_test.clj
+++ test/regex_matcher/core_test.clj
@@ -26,3 +26,41 @@
       (is (= false (accept? odd-a "aaaa")))
     (testing "Rejects 1000 a")
       (is (= false (accept? odd-a (repeat 1000 "a")))))
+
+  (def states-2 #{"S0" "S1" "S2" "S3" "S4" "S5" "SFail"})
+  (def transition-func-2 {{:state "S0", :char \a} "S1",
+                          {:state "S1", :char \b} "S2",
+                          {:state "S2", :char \b} "S3",
+                          {:state "S3", :char \a} "S4",
+                          {:state "S4", :char \b} "S5",
+                          {:state "S0", :char \b} "SFail",
+                          {:state "S1", :char \a} "SFail",
+                          {:state "S2", :char \a} "SFail",
+                          {:state "S3", :char \b} "SFail",
+                          {:state "S4", :char \a} "SFail",
+                          {:state "S5", :char \a} "SFail",
+                          {:state "S5", :char \b} "SFail",
+                          {:state "SFail", :char \a} "SFail",
+                          {:state "SFail", :char \b} "SFail"})
+  (def accept-states-2 #{"S5"})
+  (def starting-state-2 "S0")
+
+  (def abbab (->dfa states-2 transition-func-2 accept-states-2 starting-state-2))
+
+  (deftest accept-abbab
+    (testing "Accepts abbbab")
+      (is (= true (accept? abbab "abbab")))
+    (testing "Rejects empty")
+      (is (= false (accept? abbab "")))
+    (testing "Rejects a")
+      (is (= false (accept? abbab "a")))
+    (testing "Rejects b")
+      (is (= false (accept? abbab "b")))
+    (testing "Rejects five a")
+      (is (= false (accept? abbab "aaaaa")))
+    (testing "Rejects ab")
+      (is (= false (accept? abbab "ab")))
+    (testing "Rejects abab")
+      (is (= false (accept? abbab "abab")))
+    (testing "Rejects 10 ab")
+      (is (= false (accept? abbab (repeat 1000 "ab")))))
